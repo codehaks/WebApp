0c4e5e87658b79a71cef25fa65893c317af52b64
Hakim Ghods <ghods.hakim@hotmail.com>
2021-05-06 11:54:07 +0430

    Clean-up

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -23,7 +23,6 @@ namespace MyApp
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlite("Data Source=app.sqlite");
-                //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
                 options.LogTo(Console.WriteLine);
             });
 
